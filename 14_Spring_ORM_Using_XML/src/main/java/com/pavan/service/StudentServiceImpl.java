e660291d23ea37abc9aa4309416beaf75b249bc0
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-07 00:57:25 +0530

    spring Orm

--- 14_Spring_ORM_Using_XML/src/main/java/com/pavan/service/StudentServiceImpl.java
+++ 14_Spring_ORM_Using_XML/src/main/java/com/pavan/service/StudentServiceImpl.java
@@ -8,37 +8,31 @@ public class StudentServiceImpl implements IStudentService{
 
 	@Override
 	public int insert(StudentDto s) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void delete(int id) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void delete(StudentDto s) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void update(StudentDto s) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public StudentDto getStudent(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<StudentDto> getAllStudents() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
