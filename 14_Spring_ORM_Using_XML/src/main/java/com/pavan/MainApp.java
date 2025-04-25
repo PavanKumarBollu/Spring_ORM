305c7b14e4dca86001c97613f3e90fe7802f7e6e
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-25 07:56:12 +0530

    Spring ORM

--- 14_Spring_ORM_Using_XML/src/main/java/com/pavan/MainApp.java
+++ 14_Spring_ORM_Using_XML/src/main/java/com/pavan/MainApp.java
@@ -1,16 +1,35 @@
 package com.pavan;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
+
 public class MainApp 
 {
     public static void main( String[] args )
     {
        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       ((AbstractApplicationContext) context).close();
     }
 }
